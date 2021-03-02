27bffd01f9ec97c3ff29b8ffa553d3c467c53a39
Asharppen <asharppen@gmail.com>
2021-03-03 00:39:54 +0100

    Fixed world startup not reloading drop configurations properly

---
+++ Valheim.DropThat/Patches/WorldStartPatch.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using Valheim.DropThat.ConfigurationCore;
+
+namespace Valheim.DropThat.Patches
+{
+    [HarmonyPatch(typeof(FejdStartup), nameof(FejdStartup.OnWorldStart))]
+    public static class WorldStartPatch
+    {
+        private static void Postfix(ref FejdStartup __instance)
+        {
+            Log.LogDebug("World startet. Loading drop configurations.");
+            ConfigurationManager.LoadAllDropTableConfigurations();
+        }
+    }
+}
