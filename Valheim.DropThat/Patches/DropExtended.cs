857cba179a524ca01d6c63288ee0d4fd5a74fe9b
ASharpPen <asharppen@gmail.com>
2021-03-18 21:38:22 +0100

    Feature/default items (#39)
    * Added option for generating default items file
    
    * Improved config description
    
    * Reducing potential issue with mod foodprint.

--- Valheim.DropThat/Patches/DropExtended.cs
+++ Valheim.DropThat/Patches/DropExtended.cs
@@ -1,8 +1,27 @@
 ﻿
+using System.Runtime.CompilerServices;
+
 namespace Valheim.DropThat.ConfigurationTypes
 {
-    public class DropExtended : CharacterDrop.Drop
+    public class DropExtended
     {
+        private static ConditionalWeakTable<CharacterDrop.Drop, DropExtended> DropTable = new ConditionalWeakTable<CharacterDrop.Drop, DropExtended>();
+
+        public static void Set(CharacterDrop.Drop drop, DropConfiguration config)
+        {
+            DropTable.GetOrCreateValue(drop).Config = config;
+        }
+
+        public static DropExtended GetExtension(CharacterDrop.Drop drop)
+        {
+            if(DropTable.TryGetValue(drop, out DropExtended dropExtended))
+            {
+                return dropExtended;
+            }
+
+            return null;
+        }
+
         public DropConfiguration Config { get; set; }
     }
 }
