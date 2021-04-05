9c9183f5f1d97e050dea4711dcc3f87253b3d1e4
Asharppen <asharppen@gmail.com>
2021-04-05 02:09:40 +0200

    Version bump and readme

--- Valheim.DropThat/DropThatPlugin.cs
+++ Valheim.DropThat/DropThatPlugin.cs
@@ -4,7 +4,7 @@ using Valheim.DropThat.ConfigurationCore;
 
 namespace Valheim.DropThat
 {
-    [BepInPlugin("asharppen.valheim.drop_that", "Drop That!", "1.6.1")]
+    [BepInPlugin("asharppen.valheim.drop_that", "Drop That!", "1.6.2")]
     public class DropThatPlugin : BaseUnityPlugin
     {
         // Awake is called once when both the game and the plug-in are loaded
