83e49b1b1c679a131106fd4ac431eeb5bc1b4ddf
Asharppen <asharppen@gmail.com>
2021-05-31 23:22:30 +0200

    Added location sync and helper

---
+++ Valheim.DropThat/Locations/LocationHelper.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Valheim.SpawnThat.Core;
+using Valheim.SpawnThat.Reset;
+
+namespace Valheim.DropThat.Locations
+{
+    internal static class LocationHelper
+    {
+        private static Dictionary<Vector2i, ZoneSystem.LocationInstance> _locationInstances { get; set; }
+        private static Dictionary<Vector2i, SimpleLocation> _simpleLocationsByZone { get; set; }
+
+        static LocationHelper()
+        {
+            StateResetter.Subscribe(() =>
+            {
+                _locationInstances = null;
+                _simpleLocationsByZone = null;
+            });
+        }
+
+        internal static void SetLocationInstances(Dictionary<Vector2i, ZoneSystem.LocationInstance> locations)
+        {
+            _locationInstances = locations;
+        }
+
+        internal static void SetLocations(IEnumerable<SimpleLocation> locations)
+        {
+            if (_simpleLocationsByZone is null)
+            {
+#if DEBUG
+                Log.LogDebug($"Instantiating new dictionary for SimpleLocations.");
+#endif
+                _simpleLocationsByZone = new Dictionary<Vector2i, SimpleLocation>();
+            }
+
+#if DEBUG
+            Log.LogDebug($"Assigning locations.");
+#endif
+
+            foreach (var location in locations)
+            {
+                _simpleLocationsByZone[location.ZonePosition] = location;
+            }
+        }
+
+        public static SimpleLocation FindLocation(Vector3 position)
+        {
+            if (ZoneSystem.instance is null)
+            {
+                Log.LogWarning("Attempting to retrieve location before ZoneSystem is initialized.");
+                return null;
+            }
+
+            var zoneId = ZoneSystem.instance.GetZone(position);
+
+            if ((_locationInstances?.Count ?? 0) > 0)
+            {
+                if (_locationInstances.TryGetValue(zoneId, out ZoneSystem.LocationInstance defaultLocation))
+                {
+                    return new SimpleLocation
+                    {
+                        LocationName = defaultLocation.m_location?.m_prefabName ?? "",
+                        Position = defaultLocation.m_position,
+                        ZonePosition = zoneId,
+                    };
+                }
+            }
+
+            if (_simpleLocationsByZone is not null)
+            {
+                if (_simpleLocationsByZone.TryGetValue(zoneId, out SimpleLocation location))
+                {
+                    return location;
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Valheim.DropThat/Locations/SimpleLocation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace Valheim.DropThat.Locations
+{
+    internal class SimpleLocation
+    {
+        public Vector3 Position;
+
+        public Vector2i ZonePosition;
+
+        public string LocationName;
+    }
+}
